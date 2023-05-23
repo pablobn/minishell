d7e870e398af76bc743fd50ed3baf31111b72373
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-05-23 12:58:49 +0200

    Control D y C funcionales a falta de no mostrar ^C

--- src/minishell/minishell.c
+++ src/minishell/minishell.c
@@ -1,40 +1,40 @@
 #include "minishell.h"
 
-t_command	cmd;
+void	ft_handler(int sig)
+{
+	// struct termios	old_termios;
+	// struct termios	new_termios;
 
+	// tcgetattr(STDIN_FILENO, &old_termios);
+	// new_termios = old_termios;
+	// new_termios.c_lflag &= ~ECHO;
+	// tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+	// tcsetattr(STDIN_FILENO, TCSANOW, &old_termios);
+	(void) sig;
+	printf("\n");
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
 void	ft_prompt(void)
 {
-	struct termios	old_termios;
-	struct termios	new_termios;
-
-	tcgetattr(STDIN_FILENO, &old_termios);
-	new_termios = old_termios;
-	new_termios.c_lflag &= ~ECHO;
-	tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
-	cmd.command = readline(GREEN "minishell" RED "$" RESET " ");
-	add_history(cmd.command);
-	tcsetattr(STDIN_FILENO, TCSANOW, &old_termios);
+	signal(SIGINT, ft_handler);
+	g_cmd.command = readline(GREEN "minishell" RED "$" RESET " ");
+	add_history(g_cmd.command);
 }
 
 //atexit(ft_leaks);
 
-void	ft_handler(int sig)
-{
-	(void) sig;
-	ft_prompt();
-}
 
 int	main(int argc, char **argv, char **envp)
 {
-
-	signal(SIGINT, ft_handler);
 	(void) argc;
 	(void) argv;
 	(void) envp;
 	while (42)
 	{
 		ft_prompt();
-		if (!cmd.command)
+		if (!g_cmd.command)
 			return (0);
 	}
 	return (0);
