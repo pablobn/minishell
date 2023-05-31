e2a4642415910bc4de87c693fa31a9e19def6a6c
Pablo Bengoechea Navarro <pbengoec@c3r4s2.42malaga.com>
2023-05-31 15:25:58 +0200

    Pipex listo faltan más comprobaciones

--- src/minishell/pipex.c
+++ src/minishell/pipex.c
@@ -2,19 +2,27 @@
 
 void	ft_give_value(t_command *a)
 {
-	// t_command	b;
-	char **first;
-	first = malloc(sizeof(char *) * 3);
+	t_command	*b;
+	char		**first;
+	char		**second;
+
+	b = ft_calloc(1, sizeof(t_command));
+	first = ft_calloc(3, sizeof(char *));
 	first[0] = "ls";
 	first[1] = "-l";
 	first[2] = NULL;
-	// char *second[] = {"wc", "-l"};
-	// b.command = "wc";
-	// b.next = NULL;
-	// b.back = a;
-	// b.flags = second;
+	second = ft_calloc(3, sizeof(char *));
+	second[0] = "wc";
+	second[1] = "-l";
+	second[2] = NULL;
+	b->command = "wc";
+	b->next = NULL;
+	b->back = a;
+	b->out = STDOUT_FILENO;
+	b->in = STDIN_FILENO;
+	b->flags = second;
 	a->command = "ls";
-	a->next = NULL;
+	a->next = b;
 	a->back = NULL;
 	a->out = STDOUT_FILENO;
 	a->in = STDIN_FILENO;
@@ -42,44 +50,58 @@ static char	*ft_get_cmd(char *str, char *path)
 
 void	ft_execute_command(t_command *list, char **envp)
 {
-	char *path;
-	char *cmd_path;
+	char	*path;
+	char	*cmd_path;
 
 	path = getenv("PATH");
 	cmd_path = ft_get_cmd(ft_strjoin("/", list->command), path);
 	if (!cmd_path)
-		exit(1);
+		exit (1);
 	execve(cmd_path, list->flags, envp);
 }
 
-// void	ft_pipex(t_command *list)
-// {
-// 	int	tube[2];
-// 	pid_t pid;
+void	ft_pipex(t_command *list, char **envp)
+{
+	pid_t	pid;
+	int		tube[2];
 
-// 	pipe(tube);
-
-// }
+	pipe(tube);
+	pid = fork();
+	if (pid == 0)
+	{
+		close(tube[0]);
+		if (list->in != STDIN_FILENO)
+			dup2(list->in, STDIN_FILENO);
+		if (list->out != STDOUT_FILENO)
+			dup2(list->out, STDOUT_FILENO);
+		else
+			dup2(tube[1], STDOUT_FILENO);
+		close(tube[1]);
+		ft_execute_command(list, envp);
+	}
+	else
+	{
+		wait(NULL);
+		close(tube[1]);
+		dup2(tube[0], STDIN_FILENO);
+		close(tube[0]);
+	}
+}
 
 int	ft_execute_line(t_ms *ms)
 {
 	t_command	*list;
 	pid_t		pid;
-	int			j;
 
 	pid = fork();
 	if (pid == 0)
 	{
 		list = ms->list;
 		ft_give_value(list);
-		// dup2(list->out, STDOUT_FILENO);
-		// dup2(list->in, STDIN_FILENO);
-		j = 0;
 		while (list->next)
 		{
-			// ft_pipex(list);
+			ft_pipex(list, ms->envp);
 			list = list->next;
-			j++;
 		}
 		ft_execute_command(list, ms->envp);
 	}
