8b0951326703358b1d8fc6f194a67877ec834027
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-06-14 16:11:41 +0200

    soy tonto

--- src/minishell/minishell.c
+++ src/minishell/minishell.c
@@ -28,21 +28,25 @@ int	main(int argc, char **argv, char **envp)
 			g_ms->list[i] = ft_parser(g_ms->list[i]);
 			i++;
 		}
-		if (g_ms->list[0])
-		{
-			printf("Line:%s\n", g_ms->list[0]->line);
-			printf("Command_f:%s\n", g_ms->list[0]->cmd);
-			printf("Flags:%s\n", g_ms->list[0]->flags[0]);
-			printf("Out:%d\n", g_ms->list[0]->out);
-			printf("Out_f:%d\n", g_ms->list[0]->out_f);
-			printf("In:%d\n", g_ms->list[0]->in);
-			printf("In_f:%d\n", g_ms->list[0]->in_f);
-		}
+		// if (g_ms->list[0])
+		// {
+		// 	printf("Line:%s\n", g_ms->list[0]->line);
+		// 	printf("Command_f:%s\n", g_ms->list[0]->cmd);
+		// 	printf("Flags:%s\n", g_ms->list[0]->flags[0]);
+		// 	printf("Out:%d\n", g_ms->list[0]->out);
+		// 	printf("Out_f:%d\n", g_ms->list[0]->out_f);
+		// 	printf("In:%d\n", g_ms->list[0]->in);
+		// 	printf("In_f:%d\n", g_ms->list[0]->in_f);
+		// }
+		if (g_ms->list[0]->cmd)
+			ft_execute_line(g_ms);
 		i = 0;
 		if (g_ms->list)
 		{
+			printf("Existe list\n");
 			while (g_ms->list[i])
 			{
+				printf("Existe list interna\n");
 				ft_free_matrix(g_ms->list[i]->flags);
 				free(g_ms->list[i]->cmd);
 				free(g_ms->list[i]->line);
@@ -50,8 +54,6 @@ int	main(int argc, char **argv, char **envp)
 			}
 			free(g_ms->list[i]);
 		}
-		if (g_ms->list[0]->cmd)
-			ft_execute_line(g_ms);
 		//frees
 		// if (g_ms->list->out)
 		// {
