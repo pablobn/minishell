26bd0f63c84b65062e116b9b8b8855a5c645762d
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-06-20 17:58:41 +0200

    0 implementado

--- src/minishell/prompt.c
+++ src/minishell/prompt.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-t_ms	*g_ms;
+extern t_ms	*g_ms;
 
 static int	ft_is_empty(char *str)
 {
@@ -40,7 +40,14 @@ static t_command	*ft_expand(t_command *list, t_env *env, int i)
 			if (list->line[i + ++j] == '$')
 				break ;
 		temp = ft_substr(list->line, i, j);
-		if (temp[1] != '$' && temp[1])
+		if (temp[1] == '?' && !temp[2])
+		{
+			free(temp);
+			temp = ft_itoa(g_ms->status);
+			result = ft_strjoin_free(result, temp);
+			free(temp);
+		}
+		else if (temp[1] != '$' && temp[1])
 		{
 			if (temp[ft_strlen(temp) - 1] == '\\')
 				temp[ft_strlen(temp) - 1] = '\0';
